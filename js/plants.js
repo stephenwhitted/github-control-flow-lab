8f0f3b8fc041b523fb85e60c0dce329b8ecf4c83
Stephen Whitted <stephent.whitted@gmail.com>
2024-04-12 21:26:45 -0400

    commit again

--- js/plants.js
+++ js/plants.js
@@ -1,1 +1,17 @@
 console.log("Happy Plants!")
+//Part 1
+/**The area in which the plants are contained is circular, with a radius of 5 meters.
+The formula for calculating the area of a circle is PI multiplied by the radius, squared:
+const PI = 3.1415;
+const area = PI * radius * radius;
+Each plant requires a minimum space of 0.8 square meters.
+The area is starting with 20 plants.
+The plants double in number every week. */
+const PI = 3.1415
+const r = 5
+const totalArea = PI * r * r
+const maxPlants = totalArea / precspace
+const pStart = 20
+const precspace = 0.8
+const weeks = 3
+const tPlants = 2
